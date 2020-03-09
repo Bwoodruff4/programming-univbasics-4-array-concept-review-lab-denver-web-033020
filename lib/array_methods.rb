e1382c64090fc871b65322334810849b9a673db1
Blake <blakewoodruff4@gmail.com>
2020-03-09 18:51:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,11 @@ end
 
 def find_max_value(array)
   array.length.time do |index|
-    if array[index]>
+    if array[index]>array[index-1]
+      max=array[index]
+    end
+  end
+  return max
 end
 
 def find_min_value(array)
