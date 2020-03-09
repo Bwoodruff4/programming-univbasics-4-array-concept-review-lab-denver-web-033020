ab9b0c6b8687fabaecbd23b02cf0dfdb9680120b
Blake <blakewoodruff4@gmail.com>
2020-03-09 18:44:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index]==value_to_find
-      retun index
-      
+      return index
+    end
+  end
 end
 
 def find_max_value(array)
